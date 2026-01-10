47eaf1964985bfc1ab29f9377f03ee8ab5d2f02c
Vipul Kumar <email.coex@gmail.com>
2026-01-11 01:49:55 +0530

    Enhance TodayGames component by integrating Swiper for a responsive carousel display of trending athletes. Added new athlete data and utilized HideExtra for improved overflow management.

--- src/components/home-page/trending.tsx
+++ src/components/home-page/trending.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import React from "react";
 import AthletesCard from "../common/athletes-card";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import HideExtra from "../common/hide-extra";
 
 interface TrendingData {
   id: string;
@@ -39,6 +44,69 @@ const trendingItems: TrendingData[] = [
     change: 2.15,
     percentage: 85,
   },
+  {
+    id: "4",
+    image: "/icons/athletes/luka-doncic.png",
+    name: "Luka Doncic",
+    abbreviation: "DAL",
+    price: "$108.9",
+    change: -0.89,
+    percentage: 78,
+  },
+  {
+    id: "5",
+    image: "/icons/athletes/jayson-tatum.png",
+    name: "Jayson Tatum",
+    abbreviation: "BOS",
+    price: "$99.7",
+    change: 2.45,
+    percentage: 88,
+  },
+  {
+    id: "6",
+    image: "/icons/athletes/g-antetokounmpo.png",
+    name: "Giannis Antetok",
+    abbreviation: "MIL",
+    price: "$112.4",
+    change: 4.12,
+    percentage: 92,
+  },
+  {
+    id: "7",
+    image: "/icons/athletes/anthony-davis.png",
+    name: "Anthony Davis",
+    abbreviation: "LAA",
+    price: "$96.8",
+    change: -1.89,
+    percentage: 72,
+  },
+  {
+    id: "8",
+    image: "/icons/athletes/stephen-curry.png",
+    name: "Stephen Curry",
+    abbreviation: "GSW",
+    price: "$101.2",
+    change: 1.54,
+    percentage: 82,
+  },
+  {
+    id: "9",
+    image: "/icons/athletes/kevin-durant.png",
+    name: "Kevin Durant",
+    abbreviation: "PHX",
+    price: "$107.6",
+    change: -2.31,
+    percentage: 79,
+  },
+  {
+    id: "10",
+    image: "/icons/athletes/luka-doncic.png",
+    name: "Luka Doncic",
+    abbreviation: "DAL",
+    price: "$110.3",
+    change: 3.78,
+    percentage: 90,
+  },
 ];
 
 export default function TodayGames() {
@@ -47,22 +115,28 @@ export default function TodayGames() {
       <h3 className="text-text-secondary m-0 mb-2 p-0 text-[14px] font-semibold">
         Trending
       </h3>
-      <div className="m-0 flex flex-row gap-2 overflow-x-auto overflow-y-hidden p-0">
-        {trendingItems.map((item) => (
-          <div key={item.id} className="shrink-0">
-            <AthletesCard
-              key={item.id}
-              id={item.id}
-              image={item.image}
-              name={item.name}
-              abbreviation={item.abbreviation}
-              price={item.price}
-              change={item.change}
-              percentage={item.percentage}
-            />
-          </div>
-        ))}
-      </div>
+      <HideExtra>
+        <Swiper
+          spaceBetween={8}
+          slidesPerView="auto"
+          navigation
+          className="overflow-visible! pb-4!"
+        >
+          {trendingItems.map((item) => (
+            <SwiperSlide key={item.id} className="w-auto!">
+              <AthletesCard
+                id={item.id}
+                image={item.image}
+                name={item.name}
+                abbreviation={item.abbreviation}
+                price={item.price}
+                change={item.change}
+                percentage={item.percentage}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </HideExtra>
     </div>
   );
 }
